8b5b10396edc36fd9be7083c61c9e41d57a6b9a5
irsyadpahlapi <irsyadpahlapi28@gmail.com>
2017-12-19 02:17:47 -0500

    add js selasa

---
+++ selasa/exercise-2.js
@@ -0,0 +1,10 @@
+for(var i = 1; i<=100;i++){
+  if(i % 2 == 0){
+    console.log('counter sekarang = '+ i);
+    console.log('output genap');
+  }
+  else{
+    console.log('counter sekarang = '+ i);
+    console.log('output ganjil');
+  }
+}
---
+++ selasa/exercise-3.js
@@ -0,0 +1,18 @@
+for(var i =1; i<=100;i+=2){
+  console.log('counter sekarang = '+ i);
+  if(i % 3 == 0){
+    console.log('output '+i+' KELIPATAN '+i);
+  }
+}
+for(var j =1; j<=100;j+=5){
+  console.log('counter sekarang = '+ j);
+  if(j % 6 == 0){
+    console.log('output '+j+' KELIPATAN '+j);
+  }
+}
+for(var k =1; k<=100;k+=9){
+  console.log('counter sekarang = '+ k);
+  if(k % 10 == 0){
+    console.log('output '+k+' KELIPATAN '+k);
+  }
+}

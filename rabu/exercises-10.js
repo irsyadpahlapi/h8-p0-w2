37eca87f1fa1a4674796a67848340ed1883343cd
irsyadpahlapi <irsyadpahlapi28@gmail.com>
2017-12-21 10:25:24 -0500

    update tugas10 sebelumnya

--- rabu/exercises-10.js
+++ rabu/exercises-10.js
@@ -13,7 +13,7 @@ console.log("TOTAL : "+total);
 var pagar ='';
 for(var i=1; i<=10;i++){
   for(var j=0;j<=10;j++){
-    pagar = '#';
+    pagar = pagar+'#';
   }
   console.log(pagar);
   pagar = '';

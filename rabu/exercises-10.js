8ccaa7ffd9069a17917f1f1cd899f26e74f2fcf9
irsyadpahlapi <irsyadpahlapi28@gmail.com>
2017-12-21 10:05:14 -0500

    update tugas sebelumnya

--- rabu/exercises-10.js
+++ rabu/exercises-10.js
@@ -23,7 +23,7 @@ for(var i=1; i<=10;i++){
 var bintang = "";
 for(var i = 1; i<10; i++){
   for(var j= 0; j<i; j++){
-    bintang = '*';
+    bintang = bintang+'*';
   }
   console.log(bintang);
   bintang = '';
